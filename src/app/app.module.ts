8bb7259f5995a5c7f8500a5415293e9838b5f3ac
catalinito <tomascatalini@gmail.com>
2023-06-20 19:07:32 -0300

    Primer Entrega: Integrando Conocimientos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    NavbarComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/toolbar/toolbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: []
+})
+export class ToolbarComponent {
+
+  constructor() { }
+
+}
